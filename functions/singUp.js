47bc094fe8c70596ba4bde0aaa04a7d83f4b70d3
Idder Ait El Mouden <139082259+IDDER29@users.noreply.github.com>
2024-04-09 12:38:19 +0000

    edit the forms styles and fix the dites button

---
+++ functions/singUp.js
@@ -0,0 +1,14 @@
+
+function save(e) {
+    console.log("hhh");
+    window.location.href = "index.html"; // Redirect to the desired page
+  }
+  function callName() {
+    let name = document.querySelector("#name");
+    console.log(name.value);
+  }
+
+
+  let userName = document.querySelector("#name");
+let userEmail = document.querySelector("#email")
+let userPassword = document.querySelector("#password");
